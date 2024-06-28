8201f7a53d23188e839ef1245abdbe735a35b274
fridolinvii <carloichfridolin@gmail.com>
2024-06-28 15:19:26 +0000

    website so far

--- website/src/App.js
+++ website/src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
+import myImage from './images/Carlo_Seppi_18.6.24.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="content-wrapper">
+          <img src={myImage} className="left-image" alt="My Image" />
+          <div className="content">
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <p>
+              {/*  Edit <code>src/App.js</code> and save to reload. */}
+            </p>
+            <p>
+            <button
+              className="App-button"
+              onClick={() => window.open("https://github.com/fridolinvii", "_blank")}
+            >
+               <img src={require('./images/github.png')} alt="GitHub" className="button-image" />
+            </button> 
+            <button
+              className="App-button"
+              onClick={() => window.open("https://www.researchgate.net/profile/Carlo-Seppi", "_blank")}
+            >
+              <img src={require('./images/researchgate.png')} alt="ResearchGate" className="button-image" />
+            </button> 
+            <button
+              className="App-button"
+              onClick={() => window.open("https://www.linkedin.com/in/carlo-seppi-0a2222169", "_blank")}
+            >
+              <img src={require('./images/linkedin.png')} alt="LinkedIn" className="button-image" />
+</button>
+            </p>
+          </div>
+        </div>
       </header>
     </div>
   );
