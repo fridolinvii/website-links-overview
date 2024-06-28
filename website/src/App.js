5f63633cb24bb3d78326723f3f752bbdecac09b7
fridolinvii <carloichfridolin@gmail.com>
2024-06-28 16:37:01 +0000

    add some text, but not as I want it yet

--- website/src/App.js
+++ website/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import myImage from './images/Carlo_Seppi_18.6.24.png';
 import './App.css';
 
@@ -8,32 +9,50 @@ function App() {
         <div className="content-wrapper">
           <img src={myImage} className="left-image" alt="My Image" />
           <div className="content">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
             <p>
-              {/*  Edit <code>src/App.js</code> and save to reload. */}
+              <button
+                className="App-button"
+                onClick={() => window.open("https://github.com/fridolinvii", "_blank")}
+              >
+                <img src={require('./images/github.png')} alt="GitHub" className="button-image" />
+              </button>
+              <button
+                className="App-button"
+                onClick={() => window.open("https://www.researchgate.net/profile/Carlo-Seppi", "_blank")}
+              >
+                <img src={require('./images/researchgate.png')} alt="ResearchGate" className="button-image" />
+              </button>
+              <button
+                className="App-button"
+                onClick={() => window.open("https://www.linkedin.com/in/carlo-seppi-0a2222169", "_blank")}
+              >
+                <img src={require('./images/linkedin.png')} alt="LinkedIn" className="button-image" />
+              </button>
             </p>
-            <p>
-            <button
-              className="App-button"
-              onClick={() => window.open("https://github.com/fridolinvii", "_blank")}
-            >
-               <img src={require('./images/github.png')} alt="GitHub" className="button-image" />
-            </button> 
-            <button
-              className="App-button"
-              onClick={() => window.open("https://www.researchgate.net/profile/Carlo-Seppi", "_blank")}
-            >
-              <img src={require('./images/researchgate.png')} alt="ResearchGate" className="button-image" />
-            </button> 
-            <button
-              className="App-button"
-              onClick={() => window.open("https://www.linkedin.com/in/carlo-seppi-0a2222169", "_blank")}
-            >
-              <img src={require('./images/linkedin.png')} alt="LinkedIn" className="button-image" />
-</button>
-            </p>
-          </div>
+          </div>       
         </div>
+        <div className="website-text">
+          <p className="cursive-text">
+            Dr. sc. med. in Biomedical Engineering<br />
+            M. Sc. in Mathematics
+          </p>
+        </div>
+        <div className="App-center">
+        As a mathematician with a PhD in Biomedical Engineering, I possess
+strong analytical and problem-solving skills. My academic career
+has equipped me to work both independently and collaboratively to
+address complex challenges with innovative solutions. <br /><br />
+I have hands-on experience applying mathematical approaches in various practical settings, including during civil service and PhD research. I have also taught and supervised master’s and PhD students,
+helping them develop crucial skills and understanding of essential tools
+and techniques.
+{/*”Mr. Seppi demonstrated the ability to work independently with creativity and to think
+outside of the box. [...] I know him as a motivated, enthusiastic and a dedicated
+student [...] other co-workers and Ph.D. students [...] commented favourably about
+working with him.” - Certificate of employment, University of Basel
+”I liked how Carlo, kindly and patently, told me where in my code I went wrong and
+gave advice on what I could try without giving the answer. He always responded to
+emails very quickly. I really enjoyed the process and learnt a lot.” - Student feedback*/}
+           </div>
       </header>
     </div>
   );
